79f85f410ba8a1bd20a6ab1d6f23121b55f7b8c3
Peter Martin <pgmartin@peters-mbp.lan>
2022-11-06 17:39:58 -0500

    dispatcher function no longer takes type argument

--- src/components/timers/TimerProvider.js
+++ src/components/timers/TimerProvider.js
@@ -8,23 +8,19 @@ import Tabata from "./Tabata";
 export const TimerContext = createContext({});
 
 const TimerProvider = ({ children }) => {
+  // workout - hard-coded for now
+  const initial_timers = [
+    { title: "Stopwatch", component: Stopwatch, startVal: 0, endVal: 5, roundStartVal: null, roundEndVal: null, intervalStartVal: null, intervalEndVal: null, timerSecs: 5, isRunning: false, isCompleted: false },
+    { title: "Countdown", component: Countdown, startVal: 8, endVal: 0, roundStartVal: null, roundEndVal: null, intervalStartVal: null, intervalEndVal: null, timerSecs: 8, isRunning: false, isCompleted: false },     
+    { title: "XY", component: XY, startVal: 10, endVal: 0, roundStartVal: 3, roundEndVal: 1, intervalStartVal: null, intervalEndVal: null, timerSecs: 30, isRunning: false, isCompleted: false },
+    { title: "Tabata", component: Tabata, startVal: 10, endVal: 0, roundStartVal: 3, roundEndVal: 1, intervalStartVal: 5, intervalEndVal: 0, timerSecs: 45, isRunning: false, isCompleted: false },
+  ];
+
   const [count, setCount] = useState(0);
   const [round, setRound] = useState(0);
   const [interval, setInterv] = useState(0);
-
   const [remainingTime, setRemainingTime] = useState(0);
-
-  //*** active timer by index
   const [activeTimerIdx, setActiveTimerIdx] = useState(0);
-
-  // workout - hard-coded for now
-  const initial_timers = [
-    { title: "Stopwatch", component: Stopwatch, startVal: 0, endVal: 5, roundStartVal: null, roundEndVal: null, intervalStartVal: null, intervalEndVal: null, isRunning: false, isCompleted: false },
-    { title: "Countdown", component: Countdown, startVal: 8, endVal: 0, roundStartVal: null, roundEndVal: null, intervalStartVal: null, intervalEndVal: null,  isRunning: false, isCompleted: false },     
-  { title: "XY", component: XY, startVal: 10, endVal: 0, roundStartVal: 3, roundEndVal: 1, intervalStartVal: null, intervalEndVal: null, isRunning: false, isCompleted: false },
-   { title: "Tabata", component: Tabata, startVal: 10, endVal: 0, roundStartVal: 3, roundEndVal: 1, intervalStartVal: 5, intervalEndVal: 0, isRunning: false, isCompleted: false },
- ];
-
   const [timers, setTimers] = useState(initial_timers);
   const [isPaused, setPaused] = useState(false);
   const [isStopped, setStopped] = useState(true);
@@ -39,8 +35,7 @@ const TimerProvider = ({ children }) => {
       setTimers(newTs); 
   }
 
-  const dispatcher = (type) => {
-
+  const dispatcher = () => {
     if (activeTimerIdx+1 < timers.length) {
       markTimerComplete();
       setCount(timers[activeTimerIdx+1].startVal);
@@ -48,7 +43,6 @@ const TimerProvider = ({ children }) => {
       if (timers[activeTimerIdx+1].title == 'XY' || timers[activeTimerIdx+1].title == 'Tabata') {
         setRound(timers[activeTimerIdx+1].roundStartVal);
       }
-
       if (timers[activeTimerIdx+1].title == 'Tabata') { 
         setInterv(timers[activeTimerIdx+1].intervalStartVal); 
       }

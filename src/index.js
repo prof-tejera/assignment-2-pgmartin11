a8e39b1d4228513bf46a540b1a81cf4fb8756da2
Peter Martin <pgmartin@Peters-MacBook-Pro.local>
2022-11-08 10:50:27 -0500

    more code cleanup

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import "./index.css";
 import App from "./App";
-
-
-{/*
-const App = () => {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Router>
-    </>
-  );
-}
-*/}
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

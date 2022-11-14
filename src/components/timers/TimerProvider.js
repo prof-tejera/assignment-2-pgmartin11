1444b5098dfe8058f5b2e09ac27a6beb6e0cfb87
Peter Martin <pgmartin@Peters-MacBook-Pro.local>
2022-11-14 16:47:27 -0500

    removed sample workout used for testing purposes

--- src/components/timers/TimerProvider.js
+++ src/components/timers/TimerProvider.js
@@ -1,31 +1,16 @@
 import React, { useState, createContext } from 'react';
 import { timers } from '../../views/WorkoutView';
 
-/* keep these for now - remove when initial_timers structure removed */
-import Stopwatch from "./Stopwatch";
-import Countdown from "./Countdown";
-import XY from "./XY";
-import Tabata from "./Tabata";
-/************************/
-
 export const TimerContext = createContext({});
 
 
 const TimerProvider = ({ children }) => {
-  // workout - hard-coded for now
-  const initial_timers = [
-    { title: "Stopwatch", component: Stopwatch, startVal: 0, endVal: 5, roundStartVal: null, roundEndVal: null, intervalStartVal: null, intervalEndVal: null, timerSecs: 5, isRunning: false, isCompleted: false },
-    { title: "Countdown", component: Countdown, startVal: 8, endVal: 0, roundStartVal: null, roundEndVal: null, intervalStartVal: null, intervalEndVal: null, timerSecs: 8, isRunning: false, isCompleted: false },     
-    { title: "XY", component: XY, startVal: 10, endVal: 0, roundStartVal: 3, roundEndVal: 1, intervalStartVal: null, intervalEndVal: null, timerSecs: 30, isRunning: false, isCompleted: false },
-    { title: "Tabata", component: Tabata, startVal: 10, endVal: 0, roundStartVal: 3, roundEndVal: 1, intervalStartVal: 5, intervalEndVal: 0, timerSecs: 45, isRunning: false, isCompleted: false },
-  ];
-
   const [count, setCount] = useState(0);
   const [round, setRound] = useState(0);
   const [interval, setInterv] = useState(0);
   const [remainingTime, setRemainingTime] = useState(0);
   const [activeTimerIdx, setActiveTimerIdx] = useState(0);
-  const [timers, setTimers] = useState(initial_timers);
+  const [timers, setTimers] = useState([]);
   const [isPaused, setPaused] = useState(false);
   const [isStopped, setStopped] = useState(true);
 

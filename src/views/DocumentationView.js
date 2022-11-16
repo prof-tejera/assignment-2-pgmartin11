e0c96a533f743955b3bda4abb5828d1ee65b378f
Peter Martin <pgmartin@Peters-MacBook-Pro.local>
2022-11-16 13:57:09 -0500

    added back generic components documentation

--- src/views/DocumentationView.js
+++ src/views/DocumentationView.js
@@ -2,6 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import DocumentComponent from "../components/documentation/DocumentComponent";
 
+import Loading from "../components/generic/Loading";
+import DisplayTime from "../components/generic/DisplayTime";
+import DisplayRound from "../components/generic/DisplayRound";
+import TimerBtn from "../components/generic/TimerBtn";
+
 const Container = styled.div`
   display: flex;
   justify-content: center;
@@ -20,6 +25,60 @@ const Documentation = () => {
     <Container>
       <div>
         <Title>Documentation</Title>
+        <DocumentComponent
+          title="DisplayTime: show counter value in hours, minutes, seconds format"
+          component={<DisplayTime label="Counter" count={363} />}
+          propDocs={[
+            {
+              prop: "label",
+              description: "Label for the counter metric",
+              type: "string",
+              defaultValue: "N/A",
+            },
+            {
+              prop: "count",
+              description: "Counter time in seconds",
+              type: "integer",
+              defaultValue: "N/A",
+            },
+          ]}
+        />
+        <DocumentComponent
+          title="DisplayRound: display round number"
+          component={<DisplayRound round={1}/>}
+          propDocs={[
+            {
+              prop: "round",
+              description: "Represents the current round",
+              type: "integer",
+              defaultValue: "N/A",
+            },
+          ]}
+        />
+        <DocumentComponent 
+          title="TimerBtn: timer button"
+          component={<TimerBtn label="Resume" handler={f=>f} disabled={false}/>} 
+          propDocs={[
+            {
+              prop: "label",
+              description: "Button label",
+              type: "string",
+              defaultValue: "N/A",
+            },
+            {
+              prop: "handler",
+              description: "Button click handler",
+              type: "function",
+              defaultValue: "N/A",
+            },
+            {
+              prop: "disabled",
+              description: "Represents the button state, enabled or disabled",
+              type: "boolean",
+              defaultValue: "false",
+            },
+          ]}
+        />
         <DocumentComponent
           title="Stopwatch Timer"
           propDocs={[
